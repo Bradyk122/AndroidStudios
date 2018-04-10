5d3dec8809546b4551f690265ccfc3e4e79dd17d
Ryan Stapleton <rstaple@iastate.edu>
2018-04-10 13:29:13 -0500

    Calendar

---
+++ Calendar/app/src/main/java/com/example/ryanstapleton/calendar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ryanstapleton.calendar;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        CalendarView calendarView;
+        final TextView myDate;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/"+ i2 + "/" + i;
+                myDate.setText(date);
+            }
+        });
+    }
+}
