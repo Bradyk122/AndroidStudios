1a354e0876e8b0bf9652219dd0c4143ca0673cf1
bkolosik <bkolosik@iastate.edu>
2018-04-17 13:34:26 -0500

    revision 4/17/2018

--- Login/app/src/main/java/com/example/bradyslaptop/login/Login.java
+++ Login/app/src/main/java/com/example/bradyslaptop/login/Login.java
@@ -3,18 +3,24 @@ package com.example.bradyslaptop.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Login extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private TextView attempt;
     private Button login_button;
-    private String UnP = "User Admin";
     private String USERNAME = "";
     private String PASSWORD = "";
     private String USERandPASS = "";
@@ -31,7 +37,7 @@ public class Login extends AppCompatActivity {
         password = (EditText)findViewById(R.id.editText_password);
         attempt = (TextView)findViewById(R.id.textView_attempt);
         login_button = (Button)findViewById(R.id.login_button);
-
+        String UnP = readFromFile();
         Intent nextPage = new Intent(this, nextPage.class);
         USERNAME = username.getText().toString();
         PASSWORD = password.getText().toString();
@@ -52,5 +58,31 @@ public class Login extends AppCompatActivity {
     public void makeAccount(View view){
         Intent createPage = new Intent(this, createPage.class);
         startActivity(createPage);
+    }
+    public String readFromFile() {
+        String ret = "";
+        try{
+            InputStream inputStream = openFileInput("config.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String recievedString = "";
+                StringBuilder builder = new StringBuilder();
+
+                while ((recievedString = bufferedReader.readLine()) != null) {
+                    builder.append(recievedString);
+                }
+                inputStream.close();
+                ret = builder.toString();
+            }
+        }
+        catch (FileNotFoundException e){
+            Log.e("login activity", "File not found" + e.toString());
+        }
+        catch (IOException e){
+            Log.e("loginActivity", "Can not read file" + e.toString());
+        }
+        return ret;
     }
 }
--- Login/app/src/main/java/com/example/bradyslaptop/login/createPage.java
+++ Login/app/src/main/java/com/example/bradyslaptop/login/createPage.java
@@ -1,11 +1,18 @@
 package com.example.bradyslaptop.login;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class createPage extends AppCompatActivity {
 
@@ -13,17 +20,24 @@ public class createPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
+    public void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File Write Failed: " + e.toString());
+        }
+    }
+    public void save(View view){
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        String combined = user + " " + pass;
+        writeToFile(combined, this);
+        Intent back = new Intent(this, Login.class);
+        startActivity(back);
+    }
 }
